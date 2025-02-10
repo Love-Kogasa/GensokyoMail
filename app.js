b27001ec115796686bde91b8a85e38f9e3ea7ed9
misaka-fans <misaka-fans@outlook.com>
2025-02-10 13:40:52 +0800

    规范文件命名，修改结构

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ async function main( mail, throle, text, btn, term, yibian, gc, chance, upm, fil
    }
    var nmap = (await (await fetch( "./assets/json/name.json" )).json())
    var gifts = (await (await fetch( "./assets/json/gifts.json" )).json())
-   cs = (await (await fetch( "./c.json" )).json())
+   cs = (await (await fetch( "./assets/json/characters.json" )).json())
    var today = formatDate( new Date() ).date
    if( isHoliday( today ) ){
      today += " , " + getFestival( today )
@@ -250,7 +250,7 @@ async function main( mail, throle, text, btn, term, yibian, gc, chance, upm, fil
                method: "POST",
                headers: {
                   "Authorization": stringDecodeFrom9(
-                     await (await fetch( "baka1.txt" )).text() ),
+                     await (await fetch( "./assets/misc/baka1.txt" )).text() ),
                   "Content-Type": "application/json"
                },
                body: JSON.stringify({
@@ -272,7 +272,7 @@ async function main( mail, throle, text, btn, term, yibian, gc, chance, upm, fil
                   host: "mail.fds.moe",
                   from: "lovekogasa@mc.fds.moe",
                   pass: stringDecodeFrom9(
-                     await (await fetch( "baka2.txt" )).text() ),
+                     await (await fetch( "./assets/misc/baka2.txt" )).text() ),
                   to: mail.value.trim(),
                   title: "一封来自幻想乡 " + throle.value.trim() + " 的邮件",
                   content: reply.choices[0].message.content
